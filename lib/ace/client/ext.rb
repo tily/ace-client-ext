4d91bae82d48f85ea6293d5c9ea3132a5be786e0
tily <tidnlyam@gmail.com>
2014-10-29 12:40:09 +0900

    add skelton

---
+++ lib/ace/client/ext.rb
@@ -0,0 +1,9 @@
+require "ace/client/ext/version"
+
+module Ace
+  module Client
+    module Ext
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ace/client/ext/version.rb
@@ -0,0 +1,7 @@
+module Ace
+  module Client
+    module Ext
+      VERSION = "0.0.1"
+    end
+  end
+end
