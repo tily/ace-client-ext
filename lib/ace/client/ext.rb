f32948a2c8298ee278d923d968cbc0a222f85527
tily <tidnlyam@gmail.com>
2014-10-30 18:24:24 +0900

    remove trash

--- lib/ace/client/ext.rb
+++
@@ -1,9 +0,0 @@
-require "ace/client/ext/version"
-
-module Ace
-  module Client
-    module Ext
-      # Your code goes here...
-    end
-  end
-end
--- lib/ace/client/ext/version.rb
+++
@@ -1,7 +0,0 @@
-module Ace
-  module Client
-    module Ext
-      VERSION = "0.0.1"
-    end
-  end
-end
